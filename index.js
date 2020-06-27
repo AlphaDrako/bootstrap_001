c533299adc757016c7e9545ca537b9e3a088798a
Marcos Orozco Rios <marcos.orozco.rios.uni@gmail.com>
2020-06-27 15:41:19 -0500

    Primer Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// funcion se ejecuta cuando se termine de cargar el sitio
+$(function () {
+  //todos los elementos q tenga dato-toggle q tenga tooltip
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
